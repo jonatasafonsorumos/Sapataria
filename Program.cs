5dd4a948a5106e40f9a122dd8bffeccb4205f121
jonatasafonsorumos <106428780+jonatasafonsorumos@users.noreply.github.com>
2022-05-28 11:29:03 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! - MeuProjeto --- novidade");
+
+if(true){
+   Console.WriteLine("Outra linha");
+}
+
+while(true){
+   Console.WriteLine("Mais uma linha");
+}
